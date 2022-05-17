5147fa4c392b81cf090f78d815d431348f4c27fb
Jingling Jin <jljin0524@gmail.com>
2022-05-17 16:02:04 -0500

    until show the appointment on page

--- calendar_this/app/routes.py
+++ calendar_this/app/routes.py
@@ -1,7 +1,26 @@
-from flask import Blueprint, render_template
+from flask import (Blueprint, render_template)
+import os
+import psycopg2
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
+CONNECTION_PARAMETERS = {
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASS"),
+    "dbname": os.environ.get("DB_NAME"),
+    "host": os.environ.get("DB_HOST"),
+}
+
 @bp.route('/')
-def main_page():
-  return render_template('main.html')
+def main():
+  with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+    with conn.cursor() as curs:
+      curs.execute(
+        """SELECT id, name, start_datetime, end_datetime
+        FROM appointments
+        ORDER BY start_datetime;
+        """
+      )
+      rows = curs.fetchall()
+
+  return render_template('main.html', rows=rows)
