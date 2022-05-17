0ffc82413b9678488dc7b725884bc400b2cc2e11
jon <jon.halquist@gmail.com>
2022-05-17 16:44:47 -0500

    added date validation error

--- calendar_this/app/forms.py
+++ calendar_this/app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, TimeField, TextAreaField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from datetime import datetime
 
 class AppointmentForm(FlaskForm):
   name = StringField('Name', validators=[DataRequired()])
@@ -11,3 +12,9 @@ class AppointmentForm(FlaskForm):
   description = TextAreaField('Description', validators=[DataRequired()])
   private = BooleanField('Private')
   submit = SubmitField('Add Appointment')
+
+  def validate_end_date(form, field):
+    start = datetime.combine(form.start_date.data, form.start_time.data)
+    end = datetime.combine(field.data, form.end_time.data)
+    if start >= end:
+      raise ValidationError('End date/time must come after start date/time')
